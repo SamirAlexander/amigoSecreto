2d8b4465893be7dc5b0ac1cfc7bd63898028175f
SamirAlexander <samiralexander@gmail.com>
2025-03-06 16:32:38 -0500

    Se implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -2,20 +2,31 @@
 const friendsList =[];
 const regex = /^[A-Za-z]+$/;
 
-
-
-function agregarAmigo(params) {
+function agregarAmigo() {
   const nameFriend = document.getElementById('amigo').value;
+  let nameFriendCapitalized = '';
   document.getElementById('amigo').value = '';
   if (regex.test(nameFriend)) {    
     nameFriendCapitalized = nameFriend.charAt(0).toUpperCase() + nameFriend.slice(1);
-    console.log(nameFriendCapitalized);regex.test(value);
-
+    friendsList.push(nameFriendCapitalized);    
+    friendRenderList();
   }
   else {
-    alert('Ingrese un nombre válido');
-    
+    alert('Ingrese un nombre válido');    
   }
-
-
 }
+
+function friendRenderList() {
+  const title = document.getElementById('listaAmigos');
+  title.innerHTML = `<h2>Lista de amigos</h2>`;
+  const list = document.getElementById('resultado');
+  list.innerHTML = '';
+  friendsList.forEach((friend) => {
+    const item = document.createElement('li');
+    item.textContent = friend;
+    list.appendChild(item);
+  });  
+}
+
+
+
